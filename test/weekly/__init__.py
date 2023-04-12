3ec8cf1a862448febbe06792624f4841766d9343
Younger Shen <shenyangang@163.com>
2023-04-12 14:23:04 +0800

    SAVE: 保存代码

---
+++ test/weekly/__init__.py
@@ -0,0 +1,8 @@
+# Project: ZG3Doc
+# File : __init__.py
+# Date : 2023/4/12 10:40
+# Author: Younger Shen <申延刚>
+# Web: https://github.com/youngershen
+# Cell: 13811754531
+# Wechat: 13811754531
+# Email : shenyangang@163.com
