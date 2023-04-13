8cc7dbda3915147b5a96294a38cb66196231f912
Younger Shen <shenyangang@163.com>
2023-04-13 10:04:56 +0800

    SAVE: 完成第一单元日考一

--- test/daily/daily1.py
+++ test/daily/daily1.py
@@ -27,6 +27,8 @@ def case1():
     做不出来是就去看 np.arange 函数的用法
     """
     arr = np.arange(0, 10, 1)
+    print("输出创建的包含 0 - 9 的数组:")
+    print(arr)
 
     """
     2．从 arr 中提取所有奇数
@@ -37,15 +39,77 @@ def case1():
     任意数模 2 结果如果是 1 , 那么久判定它为奇数
     不理解的话就去看关于布尔检索的内容
     """
+    print_line()
+    print("arr 输出奇数:")
     print(arr[arr % 2 == 1])
 
-    # 3．将 arr 中的所有奇数替换成 -1
+    """
+    3．将 arr 中的所有奇数替换成 -1
+    
+    分析: 与第二问类似, 只是赋值直接把筛选出的
+    奇数换成了 -1
+    """
+    print_line()
+    arr[arr % 2 == 1] = -1
+    print("输出将 arr 中的奇数替换为 -1 之后的数组:")
+    print(arr)
 
-    # 4．将 arr  数组转换成 2 维数组arr2
-    # 5．获取arr2的轴的数量
-    # 6．获取arr2的第1行第2列
-    # 7．获取arr2的数组元素总个数
-    # 8．获取arr2的元素类型
+    """
+    4．将 arr  数组转换成 2 维数组arr2
+    
+    分析: 使用 reshape 改变数组形状
+    不会做就去查 reshape 的文档
+    """
+    print_line()
+    arr2 = arr.reshape((2, 5))
+    print("将 arr 变形为 (2,5) 的二维数组")
+    print(arr2)
+
+    """
+    5．获取arr2的轴的数量
+    
+    分析: 使用 ndim 参数即可直接得到 arr2
+    的轴的数量
+    """
+    print_line()
+    d = arr2.ndim
+    print("arr2 的轴的数量为:")
+    print(d)
+
+    """
+    6．获取arr2的第1行第2列
+    
+    分析: 使用下标索引即可
+    """
+    print_line()
+    print("获取 arr2 的第 1 行, 第 2 列")
+    print(arr2[1, 2])
+    print(arr2[1][2])
+    print(arr2[(1, ), (2, )])
+
+    """
+    7．获取arr2的数组元素总个数
+    
+    分析: 使用数组元素的 array.size 属性
+    即可直接得到数组中元素的个数,
+    即为形状元组中的两个数的乘积
+    """
+    print_line()
+    print("arra2 元素总个数:")
+    count = arr2.size
+    print(count)
+
+    """
+    8．获取arr2的元素类型
+    
+    分析: 使用数组元素中的 dtype 即
+    可直接得到数组元素的类型
+    """
+    print_line()
+    print("arr2 中元素的类型为:")
+    print(arr2.dtype)
+
+
     pass
 
 
@@ -159,10 +223,7 @@ def case14():
 
 
 def main():
-    case14()
-    # case13()
-    # case12()
-    # case11()
+    case1()
     pass
 
 
