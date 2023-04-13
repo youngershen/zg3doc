16926d3ce57063a3afc06d18a319acc7ef317c17
Younger Shen <shenyangang@163.com>
2023-04-13 09:46:27 +0800

    SAVE: 保存代码

--- test/daily/daily1.py
+++ test/daily/daily1.py
@@ -7,24 +7,64 @@
 # Wechat: 13811754531
 # Email : shenyangang@163.com
 import numpy as np
-
-
-def print_line(count=50):
-    line = '=' * count
-    print(line)
+from test.utils import print_line
 
 
 """
 日考一 第一套习题
+
+一定好好好练习哦 
+知识点老师都讲过的 
+不懂的随时问老师 
 """
+
+
+def case1():
+    """
+    1．创建数字从 0 到 9 的 1 维数组arr
+
+    分析: 使用 np.arange 函数直接创建,
+    做不出来是就去看 np.arange 函数的用法
+    """
+    arr = np.arange(0, 10, 1)
+
+    """
+    2．从 arr 中提取所有奇数
+    
+    分析: 数组中数据的筛选这里使用到了布尔筛选
+    方法是在方括号里放入一个返回值为
+    布尔类型的表达式, 奇数的筛选方法为
+    任意数模 2 结果如果是 1 , 那么久判定它为奇数
+    不理解的话就去看关于布尔检索的内容
+    """
+    print(arr[arr % 2 == 1])
+
+    # 3．将 arr 中的所有奇数替换成 -1
+
+    # 4．将 arr  数组转换成 2 维数组arr2
+    # 5．获取arr2的轴的数量
+    # 6．获取arr2的第1行第2列
+    # 7．获取arr2的数组元素总个数
+    # 8．获取arr2的元素类型
+    pass
+
 
 """
 日考一 第二套习题
 """
 
+
+def case2():
+    pass
+
+
 """
 日考一 第三套习题
 """
+
+
+def case3():
+    pass
 
 
 def case11():
@@ -116,8 +156,6 @@ def case14():
     """
     日习题 1.6 获取arr2的第1行第2列
     """
-
-
 
 
 def main():
---
+++ test/utils.py
@@ -0,0 +1,12 @@
+# Project: ZG3Doc
+# File : utils.py
+# Date : 2023/4/13 9:31
+# Author: Younger Shen <申延刚>
+# Web: https://github.com/youngershen
+# Cell: 13811754531
+# Wechat: 13811754531
+# Email : shenyangang@163.com
+def print_line(count=50):
+    line = '=' * count
+    print(line)
+
