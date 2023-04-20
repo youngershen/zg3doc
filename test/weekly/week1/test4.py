ccedd710e3b78250f29f8bed27dd06f7f4897213
Younger Shen <shenyangang@163.com>
2023-04-20 23:12:21 +0800

    SAVE: 保存代码

--- test/weekly/week1/test4.py
+++ test/weekly/week1/test4.py
@@ -7,16 +7,19 @@
 # Wechat: 13811754531
 # Email : shenyangang@163.com
 """
-
-2023 年 1 月
-
+专高三 周考一 2023 年 1 月
 """
 import numpy as np
 import pandas as pd
 
-# （一）使用numpy和pandas三方库等，实现以下要求，代码放在week1.py文件中：
+"""
+一 使用 numpy 和 pandas 三方库等, 实现以下要求, 代码放在 week1.py 文件中:
+"""
+
 # 1.使用["cat","dog","deer","fish","monkey"]生成一个numpy数组
-arr = np.array(["cat", "dog", "deer", "fish", "monkey"])
+a1 = np.array(["cat", "dog", "deer", "fish", "monkey"])
+print(a1)
+
 # 2.生成一个元素值全为5、长度为20的一维数组
 # arr2 = np.zeros(20, dtype=int)
 # arr2[arr2 == arr2] = 5
@@ -56,7 +59,7 @@ df8.fillna(value=df8['age'].mean(), axis=1, inplace=True)
 df8.sort_values(by='age', ascending=False, inplace=True)
 # （二）使用适当的python库进行综合数据分析，代码保存为week2.py：
 # 11.读取diamonds.csv生成数据集df，对数据集的数据质量进行评价
-df = pd.read_csv('./diamonds.csv')
+df = pd.read_csv('./data/diamonds.csv')
 # 12.对数据集中符合连续型数据的字段进行统计性描述
 df12 = df.describe()
 # 13.根据cut字段计算每一个等级（clarity字段）钻石的数量
@@ -66,12 +69,12 @@ df14 = df.groupby(by='color').agg({'carat': ['max', 'min', 'mean']})
 # 15.先按字段color、再按字段cut分组，计算carat字段的总和
 df15 = df.groupby(by=['color', 'cut'])['carat'].sum()
 # 16.筛选carat字段值大于3.5的数据，生成新的数据集，保存为excel文件
-df[df['carat'] > 3.5].to_excel('./34.xlsx')
+df[df['carat'] > 3.5].to_excel('./data/34.xlsx')
 # 17.根据price字段值进行处理，生成一个新列level，price大于等于2000时，level值设置为1，否则设置为0
 df['level'] = df.apply(lambda x: 1 if x['price'] >= 2000 else 0, axis=1)
 # 18.对第17题的数据，筛选carat字段值大于3.2、level字段值为1的数据，保存到excel文件
-df[(df['carat'] > 3.2) & (df['level'] == 1)].to_excel('./18.xlsx')
+df[(df['carat'] > 3.2) & (df['level'] == 1)].to_excel('./data/18.xlsx')
 # 19.对df数据集按照carat、price字段进行升序排序，保存到excel文件
-df.sort_values(by=['carat', 'price']).to_excel('./19.xlsx')
+df.sort_values(by=['carat', 'price']).to_excel('./data/19.xlsx')
 # 删除字段x,y,z，生成新的数据集，保存到excel文
-df.drop(columns=['x', 'y', 'z']).to_excel('./20.xlsx')
+df.drop(columns=['x', 'y', 'z']).to_excel('./data/20.xlsx')
